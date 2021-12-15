cc4227192c09ad8e8668f8c4cbfe526b7bdf203e
Leony Assis <leonyra@hotmail.com>
2021-12-15 19:54:45 -0300

    feat: add endpoint api

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require ('express')
 const path = require ('path')
+const cors = require ('cors')
 const app = express ()
 require('dotenv').config()
+const apiRouter = require('./api/routes/apiRouter')
+
+
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.use ('/app', express.static (path.join (__dirname, '/public')))
+app.use ('/api', apiRouter)
 
 var port = process.env.PORT || 3000;
 app.listen(port)
