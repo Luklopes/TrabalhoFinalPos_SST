0a9c05741e9fd44c6eae1b618b6ba6074c9cf1f0
Leony Assis <leonyra@hotmail.com>
2021-12-23 15:20:17 -0300

    feat: add seguranca, rotas de put,post,delete

--- public/app.js
+++ public/app.js
@@ -1,6 +1,33 @@
 function init() {
-    alert('aqui');
+
+    let jwtToken = localStorage.getItem('jwt');
+
+    if (!jwtToken) {
+        document.getElementById("permission-error").style.display = "inline";
+        setTimeout(() => { window.location = '/' }, 2300);
+
+    } else {
+
+        let headersList = {
+            "Accept": "*/*",
+            "Authorization": "Bearer " + `${jwtToken}`
+        }
+
+        fetch('/api/produtos', {
+            method: "GET",
+            headers: headersList
+        }).then(res => res.json())
+            .then((produtos => {
+                let textHTML = '';
+                produtos.forEach(element => {
+                    textHTML = '';
+                    textHTML += element.id + ': ' + element.descricao + ' - ' + element.marca + ': ' + element.valor + '<br>';
+                    document.getElementById('lista_produtos').innerHTML += textHTML
+                });
+            }));
+    }
 }
+
 
 
 document.onload = init();
--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-const express = require ('express')
-const path = require ('path')
-const cors = require ('cors')
-const app = express ()
 require('dotenv').config()
-const apiRouter = require('./api/routes/apiRouter')
 
+const express = require ('express');
+const path = require ('path');
+const cors = require ('cors');
+const app = express ();
+const apiRouter = require('./api/routes/apiRouter');
 
 app.use(cors());
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use ('/app', express.static (path.join (__dirname, '/public')))
-app.use ('/api', apiRouter)
+app.use ('/', express.static (path.join (__dirname, '/views/login')));
+app.use ('/app', express.static (path.join (__dirname, '/public')));
+
+app.use ('/api', apiRouter);
 
 var port = process.env.PORT || 3000;
 app.listen(port)
