d622b226135b91cfbe9bb034aa205d66f6b337cc
Leony Assis <leonyra@hotmail.com>
2022-01-23 11:26:51 -0300

    feat: alteração de produto, tratamento de crição/alteracao na mesa view

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 require('dotenv').config()
 
-const express = require ('express');
-const path = require ('path');
-const cors = require ('cors');
-const app = express ();
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const app = express();
 const apiRouter = require('./api/routes/apiRouter');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use ('/', express.static (path.join (__dirname, '/views/login')));
-app.use ('/app', express.static (path.join (__dirname, '/public')));
-app.use ('/products', express.static (path.join (__dirname, '/views/products')));
-app.use ('/products/:id', express.static (path.join (__dirname, '/views/products')));
+app.use('/api', apiRouter);
+app.use('/', express.static(path.join(__dirname, '/views/login')));
+app.use('/app', express.static(path.join(__dirname, '/public')));
+app.use('/products', express.static(path.join(__dirname, '/views/products')));
+
+app.use('/products/:id', (req, res) => {
+    express.static(path.join(__dirname, '/views/products'), {
+        setHeaders: function (res) {
+            res.set('id', req.params.id);
+        }
+    })(req,res);
+});
 
 
-app.use ('/api', apiRouter);
+
+
+
 
 var port = process.env.PORT || 3000;
 app.listen(port)
